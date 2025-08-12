e27fa72909d6b7e5e775b6b19681cb03e81ed1b5
hoobird <hulim@student.42singapore.sg>
2025-08-12 23:03:20 +0800

    Add strtol utility and implement server main logic with signal handling

---
+++ srcs/main.cpp
@@ -0,0 +1,81 @@
+#include "Server.hpp"
+#include <iostream>
+#include <sstream>
+#include <signal.h>
+#include <unistd.h>
+#include <string>
+
+static bool serverRunning = true; // Control epoll loop
+
+void    signalHandler(int signum) {
+    if (signum == SIGINT || signum == SIGQUIT) {
+        // Handle Ctrl+C signal and Ctrl+\ signal
+        serverRunning = false;
+    }
+}
+
+bool    setupSignalHandlers() {
+    struct sigaction sa;
+    sa.sa_handler = signalHandler;
+    sa.sa_flags = SA_RESTART;
+    sigemptyset(&sa.sa_mask);
+
+    if (sigaction(SIGINT, &sa, NULL) != 0 || 
+        sigaction(SIGQUIT, &sa, NULL) != 0) {
+        std::cerr << "Error: Unable to set-up signal handlers." << std::endl;
+        return false;
+    }
+    return true;
+}
+
+bool checkInputArgs(int argc, char** argv, std::string& port, std::string& password) {
+    if (argc != 3) {
+        std::cerr << "Usage: ./ircserv <port> <password>" << std::endl;
+        return false;
+    }
+    
+    port = std::string(argv[1]);
+    password = std::string(argv[2]);
+
+    if (port.empty() || password.empty()) {
+        std::cerr << "Error: Port or Password cannot be empty." << std::endl;
+        return false;
+    }
+
+    // Validate port
+    long portNum = strtol(port.c_str(), NULL, 10);
+    if (portNum <= 1023 || portNum > 65535) // Port must be between 1024 and 65535 as ports below 1024 are reserved
+    {
+        std::cerr << "Error: Port number must be between 1024 and 65535." << std::endl;
+        return false;
+    }
+
+    return true;
+}
+
+int main(int argc, char** argv) {
+    
+    std::string port;
+    std::string password;
+
+    // Register signal handlers
+    if (!setupSignalHandlers()) {
+        return -1;
+    }
+
+    // Check input arguments
+    if (!checkInputArgs(argc, argv, port, password)) {
+        std::cerr << "Error: Invalid input arguments." << std::endl;
+        return -1;
+    }
+
+    // while loop that just runs some waiting logic
+    while (serverRunning) {
+        // Simulate waiting logic
+        std::cout << "Waiting..." << std::endl;
+        sleep(1); // Sleep for 1 second
+    }
+
+
+    return 0;
+}
