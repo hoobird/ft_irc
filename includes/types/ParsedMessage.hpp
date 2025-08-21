12605088a257ff9df8014b80a9813d5eeab26d32
Hubert Lim <hubertlimshingyi@gmail.com>
2025-08-21 21:24:47 +0800

    Add ParsedMessage and ResponseType structures for message handling

---
+++ includes/types/ParsedMessage.hpp
@@ -0,0 +1,16 @@
+#ifndef PARSEDMESSAGE_HPP
+#define PARSEDMESSAGE_HPP
+
+#include <vector>
+#include <map>
+#include <string>
+
+struct ParsedMessage {
+    std::map<std::string, std::string> tags;
+    std::string source;
+    std::string command;
+    std::vector<std::string> parameters;
+    std::string trailing;
+};
+
+#endif
