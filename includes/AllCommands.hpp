ff0f950337bab8f7de12d4befb4f183c336a5b94
Hubert Lim <hubertlimshingyi@gmail.com>
2025-08-26 21:56:03 +0800

    Implement CommandMODE for Client with basic structure and error handling

--- includes/AllCommands.hpp
+++ includes/AllCommands.hpp
@@ -1,9 +1,10 @@
 #ifndef ALLCOMMANDS_HPP
 #define ALLCOMMANDS_HPP
 
+#include "commands/CommandMODE.hpp"
 #include "commands/CommandNICK.hpp"
 #include "commands/CommandPASS.hpp"
-#include "commands/CommandUSER.hpp"
 #include "commands/CommandPING.hpp"
+#include "commands/CommandUSER.hpp"
 
 #endif // ALLCOMMANDS_HPP
