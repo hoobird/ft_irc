1bd8edb1b8e1496d197f0c99174e687fa1a84466
sharpeningCode <elizabethfoo_93@hotmail.com>
2025-08-27 23:22:13 +0800

    update pseudocode

--- srcs/CommandJOIN.cpp
+++ srcs/CommandJOIN.cpp
@@ -1,115 +1,89 @@
 #include "CommandJOIN.hpp"
 
-JoinCommand::JoinCommand(void) {
+CommandJOIN::CommandJOIN(const DataStore& dataStore)
+: CommandBase(), dataStore(dataStore) {
 }
 
-JoinCommand::~JoinCommand() {
+CommandJOIN::CommandJOIN(const CommandJOIN& other)
+: CommandBase(), dataStore(other.dataStore) {
+}
+
+CommandJOIN::~CommandJOIN() {
     // Destructor logic if needed
 }
 
-void    JoinCommand::execute(Client* client, const std::vector<std::string>& params, const std::string& trailing) {
+// ERR_NOSUCHCHANNEL (403) *unsure of use case
+responseList CommandJOIN::execute(Client& client, const ParsedMessage& message) {
 
-    if (params.empty)
-    {
-        std::// server responds with ERR_NEEDMOREPARAMS (461) */
+    responseList responses;
+
+    if (message.parameters.size() < 1) {
+        // ERR_NEEDMOREPARAMS (461)
+        singleResponse resp = createSingleResponse("461", client.getSocketFdString());
+        resp["<client>"] = client.getClientPrefix();
+        resp["<reason>"] = "Not enough parameters";
+        responses.push_back(resp);
+        return responses;
     }
-        message has no parameters
 
-    /* if space present, split parameters into two
-        // first being channel names
-        // second being passwords (keys) to respective channels (if exist)
-        // store inside (std::map<std::string, std::string>) */
+    // parameters have already been split by space ('2d array')
+    if there are duplicate channel names (within parameters), only the first channel name-password pair is checked
+    if channel does not need password, but password is provided, return success; password is ignored
+    if channel is password-protected, and password is wrong, return false
 
-    /* for each channel in the command (iterate through std::map)
+    // dataStore is the only 'source of truth' or persists across commands
+    if channel name is a duplicate against mapChannelKey's [key]
+        do not overwrite existing [key]
+    if check succeeds, save channel and key respectively into mapChannelKey,
+
+    for each channel in the command (iterate through std::map)
     {
         if channel name does not exist
         {
             // server create new channel
             // server adds user to the channel (as operator)
         }
-        if channel name is invalid
-            // server responds with ERR_NOSUCHCHANNEL (403)
         else if channel name is valid
         {
-            /// how to determine CHANLIMIT? (1) go through all channels, vs (2) keep track via count
-            if client exceeds channel limit CHANLIMIT RPL_ISUPPORT,
-                // server responds with ERR_TOOMANYCHANNELS (405)
+            /* if client exceeds channel limit CHANLIMIT RPL_ISUPPORT,
+                // ERR_TOOMANYCHANNELS (405)
+                // rationale: fd exceed is caught in socket creation, setting CHANLIMIT is optional, not important */
+                // how: (1) go through all channels, (2) keep track via count */
             if channel password does not match
-                // server responds with ERR_BADCHANNELKEY (475)
+                // ERR_BADCHANNELKEY (475)
             if user is banned
-                // server responds with ERR_BANNEDFROMCHAN (474)
+                // ERR_BANNEDFROMCHAN (474)
             if (channel members) count has exceeded
-                // server responds with ERR_CHANNELISFULL (471)
+                // ERR_CHANNELISFULL (471)
+                // suggested: set 'capacity' private attribute to -1 if no limit, else set limit
+                /* TODO: set arbitrary max users value; to manage in CommandMODE (both user and server) */
             if channel is set to invite-only && user is not invited
-                // server responds with ERR_INVITEONLYCHAN (473)
+                // ERR_INVITEONLYCHAN (473)
             else
                 // server returns a JOIN message
+                // >> :elfoo!~elfoo@5626-2a9c-92a4-503c-675e.149.203.ip JOIN :#lobby
         }
-    } */
+    }
 
     if client’s JOIN command to the server is successful
     {
         responseList response;
-        singleResponse sr;
+        singleResponse resp;
 
-        if ()
-        /// server returns the following:
-        // (1) JOIN message with the client as the message <source> and
-        //	the channel they have joined as the first parameter of the message
-        // >> :elfoo!~elfoo@5626-2a9c-92a4-503c-675e.149.203.ip JOIN :#lobby
-        ??
-
-        // (2) The channel’s topic (with RPL_TOPIC (332)
+        // RPL_TOPIC (332)
         // >> :halcyon.il.us.dal.net 332 elfoo #lobby :miss you :P
-        sr["numeric"] = "332";
-        sr["client"] = client.fullname;
-        //add new sr to responseList response
-        //clear sr to reuse for below
 
-        // and optionally RPL_TOPICWHOTIME (333)
+        // (optional) RPL_TOPICWHOTIME (333)
         // >> :halcyon.il.us.dal.net 333 elfoo #lobby lilmoe!~lilmoe@fba-bb13-2cb4-6d98-137e.187.94.ip 1747255445
-        if (!params.empty()) //	no message if the channel does not have a topic
-        {
-            sr["numeric"] = "333";
-            sr["channel"] = params;
-            sr["topic"] = trailing;
-            //add new sr to responseList response
-            //clear sr to reuse for below
-        }
-        else
-        {
-            sr["channel"] = trailing;
-            sr["topic"] = "";
-        }
 
-        // (3) A list of users currently joined to the channel (with one or more RPL_NAMREPLY (353) numerics
+        // RPL_NAMREPLY (353)
         // >> :halcyon.il.us.dal.net 353 elfoo = #lobby :elfoo LDa224 Guest39645
-        //	followed by a single RPL_ENDOFNAMES (366) numeric)
-        // >> :halcyon.il.us.dal.net 366 elfoo #lobby :End of /NAMES list.
-        //  These RPL_NAMREPLY messages sent by the server MUST include the requesting client that has just joined the channel.
 
-        responseList = {
-        {
-            "<numeric>": "332",
-            "<clientsToSend>": "12,22,33",  // these are client Fds seperated by commas
-            "<client>": client.fullname,
-            "<channel>": trailing,
-            "<topic>": ?
-        },
-        {
-            "<numeric>": "353",
-            "<clientsToSend>": "12,22,33",  // these are client Fds seperated by commas
-            "<client>": client.fullname,
-            "<channel>": trailing,
-            "<nick>": client.nickname
-        },
-        {
-            "<numeric>": "366",
-            "<clientsToSend>": "12,22,33",  // these are client Fds seperated by commas
-            "<client>": client.fullname,
-            "<nick>": client.nickname,
-            "<user>": client.username,
-            "<host>": client.hostname
-        }
+        // RPL_ENDOFNAMES (366)
+        // >> :halcyon.il.us.dal.net 366 elfoo #lobby :End of /NAMES list.
     }
+}
+
+CommandBase* CommandJOIN::clone() const {
+    return new CommandJOIN(*this);
 }
