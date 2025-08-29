2ed5773dd988422324eb3308e896dec40237dd23
jellyy-t <anteo@student.42singapore.sg>
2025-08-29 23:04:54 +0800

    Add bool inviteMode, bool topicRestrict and std::set inviteList to handle INVITE and TOPIC in mode; Provides variable for certain JOINerror checking

--- includes/Channel.hpp
+++ includes/Channel.hpp
@@ -6,6 +6,7 @@
 #include <map>
 #include "Client.hpp"
 
+// Is client/user invited/banned from the channel
 enum Status {
     NONE = 0,
     INVITED,
@@ -24,31 +25,38 @@ public:
 	void addOperator(Client& client);
 	void removeOperator(Client& client);
 	bool isOperator(const Client& client) const;
+    bool isMemberInvited(const Client& client) const;
 
 	// getters
 	std::string getName() const;
 	std::string getTopic() const;
 	std::set<int> getMembers() const;
 	std::set<int> getOperators() const;
+	std::set<int> getInviteList() const;
 	std::map<std::string, std::string> getModes() const;
 	std::string getKey() const;
 	int getLimit() const;
+    bool getInviteMode() const;
+    bool getTopicRestrict() const;
 
 	// setters
 	void setName(const std::string& name);
 	void setTopic(const std::string& topic);
 	void setKey(const std::string& key);
 	void setLimit(int limit);
+    void setInviteMode(bool status);
+    void setTopicRestrict(bool status);
 
 private:
 	std::string name;
 	std::string topic;
 	std::set<int> members;
 	std::set<int> operators;
-	std::map<std::string, std::string> modes; // eliz and angelly to decide on this if u doing MODE
-	std::string key;
-	int limit; // 0 means no limit
-    std::map<int, Status> userStatus; // Key: userID, Value: status(none, invited, banned) (works cause map cannot have duplicate key, overwrites the id's status if I need update)
+    std::set<int> inviteList; // turn off inviteList when bool value is false
+	bool inviteMode;
+    bool topicRestrict;
+    std::string key; // empty string means no password
+	int limit; // -1 means no limit
 
 	// OCF
 	Channel();
