31795f36f192a7db9d580fd0d71c1904e8cc9e85
sharpeningCode <elizabethfoo_93@hotmail.com>
2025-09-03 23:18:23 +0800

    to fix 441 error numeric

--- srcs/CommandKICK.cpp
+++ srcs/CommandKICK.cpp
@@ -1,4 +1,5 @@
 #include "AllCommands.hpp"
+#include <algorithm>
 
 CommandKICK::CommandKICK(DataStore& dataStore)
 : CommandBase(), dataStore(dataStore) {
@@ -37,9 +38,9 @@ responseList CommandKICK::execute(Client& client, const ParsedMessage& message) 
         return responses;
     }
 
-    std::set<int> operatorList = dataStore.getChannel(targetChannel->getName())->getOperators();
-    std::set<int>::iterator it = operatorList.find(client.getSocketFd());
-    if (it == operatorList.end()) { // client is not channel operator
+    std::set<int> operatorList = targetChannel->getOperators();
+    std::set<int>::iterator OperatorIt = operatorList.find(client.getSocketFd()); // find method in std::set
+    if (OperatorIt == operatorList.end()) { // client is not channel operator
         // ERR_CHANOPRIVSNEEDED (482)
         singleResponse resp = createSingleResponse("482", client.getSocketFdString());
         resp["<client>"] = client.getClientPrefix();
@@ -49,34 +50,61 @@ responseList CommandKICK::execute(Client& client, const ParsedMessage& message) 
         return responses;
     }
 
-    // TODO: consider scenario where operator kicks multiple members
-    // client tries to perform a channel+nick affecting command)
-    std::set<int> memberList = dataStore.getChannel(targetChannel->getName())->getMembers();
-    std::set<int>::iterator it = memberList.find(client.getSocketFd());
-    if (it == memberList.end()) { // client is not joined to the channel
+    // client tries to perform a channel+nick affecting command
+    std::string members = intSetToCSVString(targetChannel->getMembers());
+    std::vector<std::string> memberList = split(members, ",");
+    // >> REWORK (START) 441 error numeric
+    std::vector<std::string> SuggestedMemberList = split(message.parameters[1], ",");
+    for (as we go through the vector via iterator, we obtain the client pointer) {
+        Client* targetClient = this->dataStore.getClient(message.parameters[1]);
+        if (memberIt == memberList.end()) { // client is not joined to the channel
+            // ERR_USERNOTINCHANNEL (441)
+            singleResponse resp = createSingleResponse("441", client.getSocketFdString());
+            resp["<client>"] = client.getClientPrefix();
+            resp["<nick>"] = message.parameters[1];
+            resp["<channel>"] = message.parameters[0];
+            resp["<reason>"] = "They aren't on that channel";
+            responses.push_back(resp);
+            return responses;
+        }
+    }
+    /* std::vector<std::string>::const_iterator memberIt = std::find(memberList.begin(), memberList.end(), message.parameters[1]); // find function for containers
+    std::cout << "message.parameters[1]: " << message.parameters[1] << std::endl;
+    for (std::vector<std::string>::const_iterator it = memberList.begin(); it != memberList.end(); ++it) {
+        std::cout << "Member: " << *it << std::endl;
+    }
+    if (memberIt == memberList.end()) { // client is not joined to the channel
         // ERR_USERNOTINCHANNEL (441)
         singleResponse resp = createSingleResponse("441", client.getSocketFdString());
         resp["<client>"] = client.getClientPrefix();
-        resp["<nick>"] = *it;
+        resp["<nick>"] = message.parameters[1];
         resp["<channel>"] = message.parameters[0];
         resp["<reason>"] = "They aren't on that channel";
         responses.push_back(resp);
         return responses;
-    }
+    } */
+    // REWORK (END) 441 error numeric
 
-    // // client tries to perform a channel-affecting command on a channel)
-    // if (?) { // client is not on that channel
-    //     // with ERR_NOTONCHANNEL (442)
-    //     singleResponse resp = createSingleResponse("442", client.getSocketFdString());
-    //     resp["<client>"] = client.getClientPrefix();
-    //     resp["<channel>"] = (?);
-    //     resp["<reason>"] = "You're not on that channel";
-    //     responses.push_back(resp);
-    //     return responses;
-    // }
+    // client tries to perform a channel-affecting command on a channel
+    memberIt = std::find(memberList.begin(), memberList.end(), client.getSocketFdString());
+    if (memberIt == memberList.end()) { // client is not on that channel
+        // with ERR_NOTONCHANNEL (442)
+        singleResponse resp = createSingleResponse("442", client.getSocketFdString());
+        resp["<client>"] = client.getClientPrefix();
+        resp["<channel>"] = message.parameters[0];
+        resp["<reason>"] = "You're not on that channel";
+        responses.push_back(resp);
+        return responses;
+    }
 
     /* if client’s KICK command to the server is successful, send acknowledgement */
     // remember to removeMember()!!
+    targetChannel->removeMember(client); // automatically removes operator if operator removes himself
+    singleResponse resp = createSingleResponse("KICK", client.getSocketFdString());
+    resp["<channel>"] = message.parameters[0];
+    resp["<user_sender>"] = client.getUsername();
+    resp["<comment>"] = message.trailing;
+    responses.push_back(resp);
 
     /* (optional) This message may be sent from a server to a client to notify the client that someone has been removed from a channel.
     In this case, the message <source> will be the client who sent the kick,
