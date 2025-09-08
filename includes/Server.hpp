9f2ad8ddadb4e870afe22fa87665bb18ee5c99ff
hoobird <hulim@student.42singapore.sg>
2025-09-08 19:01:19 +0800

    Refactor logging implementation by removing conditional compilation and ensuring Logger is always included in Server and main files.

--- includes/Server.hpp
+++ includes/Server.hpp
@@ -12,20 +12,12 @@
 #include "AllCommands.hpp"
 #include <string>
 #include <iostream>
-
-#ifdef LOGGER // Enable logging if LOGGER is defined
-    #include "Logger.hpp"
-#endif
-
+#include "Logger.hpp"
 
 class Server: public AuthService
 {
     public:
-        #ifndef LOGGER
-        Server(int port, const std::string& password);
-        #else
         Server(int port, const std::string& password, Logger& logger);
-        #endif
         ~Server();
 
         void start();
@@ -57,10 +49,7 @@ class Server: public AuthService
         CommandFactory cmdFactory;
         CommandHandler cmdHandler;
         MessageBuilder msgBuilder;
-
-        #ifdef LOGGER
         Logger& logger; // Reference to Logger instance
-        #endif
 
         // Useless OCF
         Server();
