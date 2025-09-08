ffdbdad57ccbf6087a971e64e54ec5eed72ffe41
jellyy-t <anteo@student.42singapore.sg>
2025-09-08 23:22:36 +0800

    Resolve stash conflict

--- srcs/CommandMODE.cpp
+++ srcs/CommandMODE.cpp
@@ -15,11 +15,11 @@ CommandMODE::~CommandMODE()
 // 461 ERR_NEEDMOREPARAMS (done)
 // 501 ERR_UMODEUNKNOWNFLAG (done)
 // 403 ERR_NOSUCHCHANNEL (done)
+// 482 ERR_CHANOPRIVSNEEDED (done)
 // 442 ERR_NOTONCHANNEL (Returned when a client tries to perform a channel-affecting command on a channel which the client isn’t a part of.)
 // 401 ERR_NOSUCHNICK (Indicates that no client can be found for the supplied nickname. The text used in the last param of this message may vary.)
 // 324 RPL_CHANNELMODEIS (sent to a client to inform them of the currently active modes of a channel)
 // 472 ERR_UNKNOWNMODE (Indicates that a mode character used by a client is not recognized by the server. The text used in the last param of this message may vary.)
-// 482 ERR_CHANOPRIVSNEEDED (Indicates that a command failed because the client does not have the appropriate channel privileges. This numeric can apply for different prefixes such as halfop, operator, etc. The text used in the last param of this message may vary.)
 
 // 467 ERR_KEYSET (Implemented for classic RFC compliant server, where if a key is set already, it needs to be removed first, dalnet simply overwrites with +k if pass was set alr)
 // 346 RPL_INVITELIST (under client protocol)
@@ -43,6 +43,7 @@ responseList CommandMODE::execute(Client& client, const ParsedMessage& message) 
         responses.push_back(resp);
         return responses;
     }
+
     // we dont handle client also
     // 501 ERR_UMODEUNKNOWNFLAG
     if (message.parameters[0][0] != '#') {
@@ -52,6 +53,7 @@ responseList CommandMODE::execute(Client& client, const ParsedMessage& message) 
         responses.push_back(resp);
         return responses;
     }
+
     const std::string channelName = message.parameters[0];
     Channel* targetChannel = dataStore.getChannel(channelName);
     if (!targetChannel) {
@@ -63,6 +65,24 @@ responseList CommandMODE::execute(Client& client, const ParsedMessage& message) 
         responses.push_back(resp);
         return responses;
     }
+
+    // Is query
+    if (message.parameters.size() == 1) {
+        // client doesn't have to be in channel or operator
+    }
+    // Setting mode
+    else {
+        if (targetChannel->isOperator(client) == false) {
+            // 482 ERR_CHANOPRIVSNEEDED
+            singleResponse resp = createSingleResponse("482", clientFdStr);
+            resp["<client>"] = clientNick;
+            resp["<channel>"] = channelName;
+            resp["<reason>"] = "You're not channel operator";
+            responses.push_back(resp);
+            return responses ;
+        }
+    }
+
     // WIP (JUST A FORMAT)
     // if ("-")
         // loop through string?
