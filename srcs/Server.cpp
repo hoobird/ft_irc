c061f05f982fe14a2e39972deb036f3d3fe845f1
sharpeningCode <elizabethfoo_93@hotmail.com>
2025-09-08 19:24:50 +0800

    implement CommandQUIT

--- srcs/Server.cpp
+++ srcs/Server.cpp
@@ -79,8 +79,7 @@ void Server::setupCommandFactory()
     cmdFactory.registerCommand("TOPIC", new CommandTOPIC(dataStore));
     cmdFactory.registerCommand("KICK", new CommandKICK(dataStore));
     cmdFactory.registerCommand("INVITE", new CommandINVITE(dataStore));
-    // cmdFactory.registerCommand("PART", new PartCommand());
-    // cmdFactory.registerCommand("QUIT", new QuitCommand());
+    cmdFactory.registerCommand("QUIT", new CommandQUIT(dataStore, networkMan));
 }
 
 void Server::handleNewConnection()
