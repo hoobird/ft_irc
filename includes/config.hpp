e968912566326b48f2c367190779c9bd76885478
jellyy-t <anteo@student.42singapore.sg>
2025-09-10 21:06:28 +0800

    Add CHANMODES to config.hpp and change limits struct to all caps

--- includes/config.hpp
+++ includes/config.hpp
@@ -4,13 +4,25 @@
 
 // rationale: fd exceed is caught in socket creation, setting CHANLIMIT is optional, not important
 
-struct limits
+struct LIMITS
 {
     static const int CLIENT_MAX_CHANNEL = 10;
     static const int CHANNEL_MAX_CLIENT = 50; // TODO: Look into server's client limit.
-
-
 } ;
 
+struct CHANMODES
+{
+    static const std::string CAT_A; // change list/address
+    static const std::string CAT_B; // take params for both "+/-"
+    static const std::string CAT_C; // only take param for "+"
+    static const std::string CAT_D; // does not take params
+} ;
+
+const std::string CHANMODES::CAT_A = "";
+const std::string CHANMODES::CAT_B = "ko";
+const std::string CHANMODES::CAT_C = "l";
+const std::string CHANMODES::CAT_D = "it";
+
+//look into enum for CHANMODES
 
 #endif
